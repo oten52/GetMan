37a52acfed11e5fe148a9c9e58fc2b7d5ec319c2
Orçun Ten <orcun.ten@niltekyazilim.com.tr>
2024-12-11 15:19:32 +0300

    Clean

--- OtenTool.PostmanCore/obj/Debug/net8.0-windows/OtenTool.PostmanCore.AssemblyInfo.cs
+++ OtenTool.PostmanCore/obj/Debug/net8.0-windows/OtenTool.PostmanCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OtenTool.PostmanCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fbbbb82219d890a89cc18cce703a0f87b533a38")]
 [assembly: System.Reflection.AssemblyProductAttribute("OtenTool.PostmanCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OtenTool.PostmanCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
